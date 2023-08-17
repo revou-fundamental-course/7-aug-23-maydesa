36c4c5034c1523b3db3af444dc0f4a1e7c1ef19d
mayde <58660049+maydesa@users.noreply.github.com>
2023-08-17 21:30:51 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Menghitung luas segitiga
   const hitungLuasButton = document.getElementById("hitung-luas");
   const hasilOutputLuas = document.getElementById("hasil-output-luas");
+  const resetLuasButton = document.getElementById("reset-luas-button");
 
   hitungLuasButton.addEventListener("click", function () {
     const tinggi = parseFloat(document.getElementById("tinggi-input-luas").value);
@@ -13,9 +14,16 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Luas Segitiga: " + luas);
   });
 
+  resetLuasButton.addEventListener("click", function () {
+    document.getElementById("tinggi-input-luas").value = "";
+    document.getElementById("alas-input-luas").value = "";
+    hasilOutputLuas.value = "";
+  });
+
   // Menghitung keliling segitiga
   const hitungKelilingButton = document.getElementById("hitung-keliling");
   const hasilOutputKeliling = document.getElementById("hasil-output-keliling");
+  const resetKelilingButton = document.getElementById("reset-keliling-button");
 
   hitungKelilingButton.addEventListener("click", function () {
     const sisi = parseFloat(document.getElementById("sisi-input-keliling").value);
@@ -26,4 +34,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     console.log("Keliling Segitiga: " + keliling);
   });
+
+  resetKelilingButton.addEventListener("click", function () {
+    document.getElementById("sisi-input-keliling").value = "";
+    document.getElementById("alas-input-keliling").value = "";
+    hasilOutputKeliling.value = "";
+  });  
 });
